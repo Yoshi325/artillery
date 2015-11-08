ab19c246b911e0b3425ad7e83a89716c4be7c19b
Charles Yost <charles.yost@binarydefense.com>
2015-11-08 16:44:51 -0500

    Move some scripts to a more logical location.

--- artillery/scripts/restart_server.py
+++ artillery/scripts/restart_server.py
@@ -1,18 +1,21 @@
-#!/usr/bin/python
-#
-# restart artillery
-#
-#
-import subprocess
+#!/usr/bin/env python
+"""
+    Restart Artillery
+"""
+
+from __future__ import print_function
 import os
-import signal
-from src.core import *
+import subprocess
+
+from artillery.core import kill_artillery
+from artillery.core import grab_time
+from artillery.core import write_log
 
 proc = subprocess.Popen("ps -A x | grep artiller[y]", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 # kill running instance of artillery
 kill_artillery()
 
-print "[*] %s: Restarting Artillery Server..." % (grab_time())
+print("[*] %s: Restarting Artillery Server..." % grab_time())
 if os.path.isfile("/var/artillery/artillery.py"):
     write_log("[*] %s: Restarting the Artillery Server process..." % (grab_time()))
     subprocess.Popen("python /var/artillery/artillery.py &", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
